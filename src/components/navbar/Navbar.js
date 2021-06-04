8110cc96e022d7db1230cc3f0123460fed614703
Diego <henriquez.dp@gmail.com>
2021-06-03 23:00:56 -0400

    cambios en el componente navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,36 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { MenuItems } from './MenuItems';
 
-class Navbar extends Component {
-  state = { clicked: false };
-  handleClick = () => {
-    this.setState({ clicked: !this.state.clicked });
+const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
   };
-  render() {
-    return (
-      <nav className="navbarItems">
-        <div className="navbar-logo">
-          Logo <i className="fas fa-code"></i>
-        </div>
-        <div className="menu-icon" onClick={this.handleClick}>
-          <i
-            className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}
-          ></i>
-        </div>
-        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-          {MenuItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <a className={item.cName} href={item.url}>
-                  {item.titulo}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    );
-  }
-}
+
+  return (
+    <nav className="navbarItems">
+      <div className="navbar-logo">
+        Logo <i className="fas fa-code"></i>
+      </div>
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+      </div>
+      <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <a className={item.cName} href={item.url}>
+                {item.titulo}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
 export default Navbar;
