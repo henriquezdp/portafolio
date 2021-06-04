9f378850d72fec3cf5147cc03f4a796d678ec91d
Diego <henriquez.dp@gmail.com>
2021-06-03 20:13:45 -0400

    se agrega navbar

---
+++ src/components/portada/Portada.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Portada.css';
+
+const Portada = () => {
+  return (
+    <div className="portada-container">
+      <h1>Diego Henríquez </h1>
+      <p>Frontend Developer</p>
+    </div>
+  );
+};
+export default Portada;
