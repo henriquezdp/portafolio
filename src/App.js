9088ee207af0dcb7eac6351c6f9a9893cdd1a561
Diego <henriquez.dp@gmail.com>
2021-06-03 20:14:59 -0400

    se agrega navbar y portada mas fuentes e iconos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar.js';
+import Portada from './components/portada/Portada.js';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Portada />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    titulo: 'Inicio',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    titulo: 'Tecnologías',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    titulo: 'Portafolio',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    titulo: 'Contacto',
+    url: '#',
+    cName: 'nav-links',
+  },
+];
