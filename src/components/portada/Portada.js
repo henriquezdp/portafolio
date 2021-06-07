caa8108256ec851e874bbcd62aa0f29644bb56f2
Diego <henriquez.dp@gmail.com>
2021-06-07 19:42:26 -0400

    cambios en los estilos de la portada y carrusel

--- src/components/portada/Portada.js
+++ src/components/portada/Portada.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Portada.css';
-// import ft2 from '../../assets/ft2.jpg';
+import codigo from '../../assets/codigo.jpg';
+import ft2 from '../../assets/ft2.jpg';
 
 const Portada = () => {
   return (
     <div className="portada-container">
-      {/* <img className="imagen" src={ft2} /> */}
-      <h1>Diego Henríquez </h1>
-      <p>Frontend Developer</p>
+      <img className="imagen" src={ft2} />
+      <img className="fondo" src={codigo} />
+      <div className="titulo-container">
+        <h1>Diego Henríquez </h1>
+        <p>Frontend Developer</p>
+      </div>
     </div>
   );
 };
