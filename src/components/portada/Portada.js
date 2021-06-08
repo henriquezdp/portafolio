9d48b3edf98a489f144d5d310fc828149cd6152e
Diego <henriquez.dp@gmail.com>
2021-06-08 18:00:16 -0400

    cambios de estilos en la portada

--- src/components/portada/Portada.js
+++ src/components/portada/Portada.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import './Portada.css';
 import codigo from '../../assets/codigo.jpg';
 import ft2 from '../../assets/ft2.jpg';
@@ -6,13 +7,16 @@ import ft2 from '../../assets/ft2.jpg';
 const Portada = () => {
   return (
     <div className="portada-container">
-      <img className="imagen" src={ft2} />
-      <img className="fondo" src={codigo} />
-      <div className="titulo-container">
-        <h1>Diego Henríquez </h1>
-        <p>Frontend Developer</p>
+      <img className="fondo" src={codigo} alt="_blank" />
+      <div className="shadow-container">
+        <img className="imagen" src={ft2} alt="_blank" />
+        <div className=" titulo-container">
+          <h1>Diego Henríquez </h1>
+          <p>Frontend Developer</p>
+        </div>
       </div>
     </div>
   );
 };
+
 export default Portada;
